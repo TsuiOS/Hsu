6d734d45c5f35ac7521ab74bfa1e3cc0945625cc
Hsu <xuning@tsuios.com>
2016-01-28 16:53:53 +0800

    设置新特性的 cell

--- HsuSina/Classes/View(视图和控制器)/Main/NewFeature/XNNewFeatureViewController.swift
+++ HsuSina/Classes/View(视图和控制器)/Main/NewFeature/XNNewFeatureViewController.swift
@@ -41,7 +41,7 @@ class XNNewFeatureViewController: UICollectionViewController {
         super.viewDidLoad()
 
         // Register cell classes
-        self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: XNCellReuseIdentifier)
+        self.collectionView!.registerClass(XNNewFeatureCell.self, forCellWithReuseIdentifier: XNCellReuseIdentifier)
 
     }
 
@@ -52,13 +52,54 @@ class XNNewFeatureViewController: UICollectionViewController {
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XNCellReuseIdentifier, forIndexPath: indexPath)
-    
-        cell.backgroundColor = indexPath.item % 2 == 0 ? UIColor.whiteColor() : UIColor.darkGrayColor()
-
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(XNCellReuseIdentifier, forIndexPath: indexPath) as! XNNewFeatureCell
+        
+        cell.imageIndex = indexPath.item
     
         return cell
     }
+}
 
+// MARK : - 新特性 cell
+private class XNNewFeatureCell: UICollectionViewCell {
     
+    ///  图像属性
+    private var imageIndex: Int = 0 {
+        didSet {
+            iconView.image = UIImage(named: "new_feature_\(imageIndex + 1)")
+            print("new_feature_\(imageIndex + 1)")
+        }
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    ///  设置界面
+    private func setupUI() {
+        //添加控件
+        addSubview(iconView)
+        addSubview(startButton)
+        
+        //指定位置
+        iconView.frame = bounds
+        
+        startButton.snp_makeConstraints { (make) -> Void in
+            make.centerX.equalTo(self.snp_centerX)
+            make.bottom.equalTo(self.snp_bottom).multipliedBy(0.7)
+        }
+    
+    
+    }
+    // MARK : - 懒加载控件
+    /// 图像
+    private lazy var iconView: UIImageView = UIImageView()
+    ///  开始按钮
+    private lazy var startButton: UIButton = UIButton(title: "开始体验", color:  UIColor.whiteColor(), imageName: "new_feature_finish_button")
+
 }
