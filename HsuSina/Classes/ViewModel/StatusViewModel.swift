0532b8d5749ca4cce0887e961e1b208cb0fac54d
Hsu <xuninghsu@gmail.com>
2016-02-06 14:21:56 +0800

    转发微博的配图视图模型数组

--- HsuSina/Classes/ViewModel/StatusViewModel.swift
+++ HsuSina/Classes/ViewModel/StatusViewModel.swift
@@ -57,23 +57,25 @@ class StatusViewModel: CustomStringConvertible {
     }
     
     /// 缩略图 URL 数组
+    /// 原创微博,可以有图,可以没有图
+    /// 转发微博,一定没有图 retweeted_status中可以有配图,也可以没有配图
     var thumbnailUrls: [NSURL]?
 
     ///  构造函数
     init(status: XNStatus) {
         self.status = status
         
-        print(status.pic_urls?.count)
         // 根据模型.来创建缩略图的数组
-        if status.pic_urls?.count > 0 {
+        if let urls = status.retweeted_status?.pic_urls ?? status.pic_urls {
         
             //创建缩略图数组
             thumbnailUrls = [NSURL]()
             
             // 遍历字典数组
-            for dict in status.pic_urls! {
+            for dict in urls {
             
                 let url = NSURL(string: dict["thumbnail_pic"]!)
+                // 相信服务器返回的 url 字符串一定能够生成
                 thumbnailUrls?.append(url!)
             }
         }
